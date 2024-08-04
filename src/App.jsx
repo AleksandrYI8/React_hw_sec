16b98cb104b6223706df12fd756643447fe22db2
Yunus <ibadovunus55@gmail.com>
2024-08-05 04:26:46 +0500

    fix

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import Layout from "./Layout/Layout"
+
+
+function App() {
+
+  return (
+    <>
+
+    <Layout>
+      <div className="wrap">
+    <div className="sec_one"></div>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+    <h1>live</h1>
+      </div>
+      </Layout>     
+    </>
+  )
+}
+
+export default App
---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+
+const Layout = ({ children }) => {
+
+  return (
+    <>
+      <header>
+        <div className="left">
+          <img src="/logo.svg" alt="" />
+          <ul>
+            <li>
+              <a href="#">Каталог</a>
+            </li>
+            <li>
+              <a href="#">Новости</a>
+            </li>
+            <li>
+              <a href="#">Доставка</a>
+            </li>
+            <li>
+              <a href="#">О нас</a>
+            </li>
+            <li>
+              <a href="#">Контакты</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="right">
+          <button>
+            <img src="/like_btn.svg" alt="" />
+          </button>
+          <button>
+          <img src="/person_btn.svg" alt="" />
+          </button>
+          <button>
+          <img src="/basket_btn.svg" alt="" />
+          </button>
+        </div>
+      </header>
+      {children}
+      <footer>
+
+      </footer>
+    </>
+  )
+}
+
+export default Layout
+
